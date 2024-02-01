72a18c33b4573ff5b6b000959f0cc65f680ac9d0
vanezaratee <a01799051@tec.mx>
2024-02-01 12:30:35 -0600

    primer Commit

---
+++ ciclowhile.py
@@ -0,0 +1,9 @@
+def main():
+    entrada = input("ingresa el comando: ")
+    while entrada == "a" :
+        print("avanzamos")
+        entrada = input("ingresa el comando: ")
+
+    print("paramos")
+
+main()
